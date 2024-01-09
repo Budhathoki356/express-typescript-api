52b1f8450d12f89bbedc5c71220c403956cb36d6
Eklal Budhathoki <budhathoki1418@gmail.com>
2024-01-09 15:42:58 +0545

    refact(server): LogLevel is deprecated

--- src/utils/db.server.ts
+++ src/utils/db.server.ts
@@ -1,14 +1,13 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 
 let $db: PrismaClient;
-const log: Prisma.LogLevel[] = ['info'];
 
 declare global {
   var __db: PrismaClient | undefined;
 }
 
 if (!global.__db) {
-  global.__db = new PrismaClient({ log });
+  global.__db = new PrismaClient({ log: ['info'] });
 }
 
 $db = global.__db;
